15ee2732bf5ff64ab94c183dbd731fb70770b67e
Shannon Jacobson <tuf19456@temple.edu>
2018-04-07 13:04:22 -0400

    dash fix

--- Views/DashboardScreen.js
+++ Views/DashboardScreen.js
@@ -51,11 +51,16 @@ export default class DashboardScreen extends React.Component {
     });
 
     if (value == true) {
+      //rider true driver false
       //Perform any task here which you want to execute on Switch ON event.
       //  Alert.alert("Rider status selected.");
+      //list of rides does not work
+
       this.setState({ disabled: false });
     } else {
       //Perform any task here which you want to execute on Switch OFF event.
+      //find a ride does not work
+      //schedule a ride does not work
       Alert.alert(
         "Driver Status Selected",
         "Would you like to fill out your schedule?",
@@ -81,130 +86,225 @@ export default class DashboardScreen extends React.Component {
 
   render() {
     const { goBack } = this.props.navigation;
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <ImageBackground
-          source={background}
-          style={styles.background}
-          resizeMode="cover"
+    if (this.state.SwitchOnValueHolder == true) {
+      return (
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
         >
-          <View
-            style={{
-              flex: 1,
-              justifyContent: "center",
-              alignItems: "center",
-              padding: 20,
-              marginTop: 20
-            }}
+          <ImageBackground
+            source={background}
+            style={styles.background}
+            resizeMode="cover"
           >
             <View
               style={{
                 flex: 1,
-                paddingHorizontal: 50,
-                paddingTop: 10,
-                borderRadius: 10,
-                backgroundColor: "white"
+                justifyContent: "center",
+                alignItems: "center",
+                padding: 20,
+                marginTop: 20
               }}
             >
-              <Text
-                style={{
-                  color: "darkred",
-                  fontFamily: "Futura",
-                  fontSize: 30,
-                  paddingBottom: 10
-                }}
-              >
-                Dashboard
-              </Text>
               <View
                 style={{
-                  alignItems: "center",
-                  justifyContent: "center",
-                  flexDirection: "row"
+                  flex: 1,
+                  paddingHorizontal: 50,
+                  paddingTop: 10,
+                  borderRadius: 10,
+                  backgroundColor: "white"
                 }}
               >
-                <Text style={{ marginRight: 10 }}>Driver</Text>
-                <Switch
-                  onValueChange={value => this.ShowAlert(value)}
-                  activeText={""}
-                  inActiveText={""}
-                  disabled={false}
-                  circleSize={30}
-                  barHeight={30}
-                  circleBorderWidth={3}
-                  backgroundActive={"darkred"}
-                  backgroundInactive={"#003399"}
-                  circleActiveColor={"#cc0000"}
-                  circleInActiveColor={"#1a75ff"}
-                  style={{ transform: [{ scaleX: 10 }, { scaleY: 0.8 }] }}
-                  value={this.state.SwitchOnValueHolder}
-                />
-                <Text style={{ marginLeft: 10 }}>Rider</Text>
+                <Text
+                  style={{
+                    color: "darkred",
+                    fontFamily: "Futura",
+                    fontSize: 30,
+                    paddingBottom: 10
+                  }}
+                >
+                  Dashboard
+                </Text>
+                <View
+                  style={{
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "row"
+                  }}
+                >
+                  <Text style={{ marginRight: 10 }}>Driver</Text>
+                  <Switch
+                    onValueChange={value => this.ShowAlert(value)}
+                    activeText={""}
+                    inActiveText={""}
+                    disabled={false}
+                    circleSize={30}
+                    barHeight={30}
+                    circleBorderWidth={3}
+                    backgroundActive={"darkred"}
+                    backgroundInactive={"#003399"}
+                    circleActiveColor={"#cc0000"}
+                    circleInActiveColor={"#1a75ff"}
+                    style={{ transform: [{ scaleX: 10 }, { scaleY: 0.8 }] }}
+                    value={this.state.SwitchOnValueHolder}
+                  />
+                  <Text style={{ marginLeft: 10 }}>Rider</Text>
+                </View>
               </View>
             </View>
-          </View>
-          <View style={{ flex: 5, marginTop: 10 }}>
-            <View style={[styles.buttonContainer, { marginTop: 0 }]}>
-              <Button
-                title="Find a Ride"
-                onPress={() =>
-                  this.props.navigation.navigate("Location", {
-                    TextEmail: this.props.navigation.state.params.TextEmail,
-                    FindOrSchedule: "Find",
-                    Status: this.state.SwitchOnValueHolder
-                  })
-                }
-                color="darkred"
-                disabled={this.state.disabled}
-              />
-            </View>
+            <View style={{ flex: 5, marginTop: 10 }}>
+              <View style={[styles.buttonContainer, { marginTop: 0 }]}>
+                <Button
+                  title="Find a Ride"
+                  onPress={() =>
+                    this.props.navigation.navigate("Location", {
+                      TextEmail: this.props.navigation.state.params.TextEmail,
+                      Status: this.state.SwitchOnValueHolder
+                    })
+                  }
+                  color="darkred"
+                  disabled={this.state.disabled}
+                />
+              </View>
 
-            <View style={[styles.buttonContainer, { marginTop: 0 }]}>
-              <Button
-                title="Schedule a Ride"
-                onPress={() =>
-                  this.props.navigation.navigate("Location", {
-                    TextEmail: this.props.navigation.state.params.TextEmail,
-                    FindOrSchedule: "Schedule",
-                    Status: this.state.SwitchOnValueHolder
-                  })
-                }
-                color="darkred"
-                disabled={this.state.disabled}
-              />
-            </View>
+              <View style={[styles.buttonContainer, { marginTop: 0 }]}>
+                <Button
+                  title="Schedule a Ride"
+                  onPress={() =>
+                    this.props.navigation.navigate("Location", {
+                      TextEmail: this.props.navigation.state.params.TextEmail,
+                      Status: this.state.SwitchOnValueHolder
+                    })
+                  }
+                  color="darkred"
+                  disabled={this.state.disabled}
+                />
+              </View>
+              <View style={[styles.buttonContainer, { marginTop: 0 }]}>
+                <Button
+                  title="Profile"
+                  onPress={() =>
+                    this.props.navigation.navigate("Profile", {
+                      TextEmail: this.props.navigation.state.params.TextEmail
+                    })
+                  }
+                  color="darkred"
+                />
+              </View>
 
-            <View style={[styles.buttonContainer, { marginTop: 0 }]}>
-              <Button
-                title="List of Rides"
-                onPress={() =>
-                  this.props.navigation.navigate("RideList", {
-                    TextEmail: this.props.navigation.state.params.TextEmail
-                  })
-                }
-                color="darkred"
-                disabled={!this.state.disabled}
-              />
+              <View style={[styles.buttonContainer, { marginTop: 0 }]}>
+                <Button
+                  title="Logout"
+                  onPress={this.deleteToken.bind(this)}
+                  color="darkred"
+                />
+              </View>
             </View>
+          </ImageBackground>
+        </View>
+      );
+    } else {
+      //driver render
+      return (
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <ImageBackground
+            source={background}
+            style={styles.background}
+            resizeMode="cover"
+          >
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                padding: 20,
+                marginTop: 20
+              }}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  paddingHorizontal: 50,
+                  paddingTop: 10,
+                  borderRadius: 10,
+                  backgroundColor: "white"
+                }}
+              >
+                <Text
+                  style={{
+                    color: "darkred",
+                    fontFamily: "Futura",
+                    fontSize: 30,
+                    paddingBottom: 10
+                  }}
+                >
+                  Dashboard
+                </Text>
+                <View
+                  style={{
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "row"
+                  }}
+                >
+                  <Text style={{ marginRight: 10 }}>Driver</Text>
+                  <Switch
+                    onValueChange={value => this.ShowAlert(value)}
+                    activeText={""}
+                    inActiveText={""}
+                    disabled={false}
+                    circleSize={30}
+                    barHeight={30}
+                    circleBorderWidth={3}
+                    backgroundActive={"darkred"}
+                    backgroundInactive={"#003399"}
+                    circleActiveColor={"#cc0000"}
+                    circleInActiveColor={"#1a75ff"}
+                    style={{ transform: [{ scaleX: 10 }, { scaleY: 0.8 }] }}
+                    value={this.state.SwitchOnValueHolder}
+                  />
+                  <Text style={{ marginLeft: 10 }}>Rider</Text>
+                </View>
+              </View>
+            </View>
+            <View style={{ flex: 5, marginTop: 10 }}>
+              <View style={[styles.buttonContainer, { marginTop: 0 }]}>
+                <Button
+                  title="List of Rides"
+                  onPress={() =>
+                    this.props.navigation.navigate("RideList", {
+                      TextEmail: this.props.navigation.state.params.TextEmail
+                    })
+                  }
+                  color="darkred"
+                  disabled={!this.state.disabled}
+                />
+              </View>
 
-            <View style={[styles.buttonContainer, { marginTop: 0 }]}>
-              <Button
-                title="Profile"
-                onPress={() =>
-                  this.props.navigation.navigate("Profile", {
-                    TextEmail: this.props.navigation.state.params.TextEmail
-                  })
-                }
-                color="darkred"
-              />
-            </View>
+              <View style={[styles.buttonContainer, { marginTop: 0 }]}>
+                <Button
+                  title="Profile"
+                  onPress={() =>
+                    this.props.navigation.navigate("Profile", {
+                      TextEmail: this.props.navigation.state.params.TextEmail
+                    })
+                  }
+                  color="darkred"
+                />
+              </View>
 
-            <View style={[styles.buttonContainer, { marginTop: 0 }]}>
-              <Button title="Logout" onPress={this.deleteToken.bind(this)} />
+              <View style={[styles.buttonContainer, { marginTop: 0 }]}>
+                <Button
+                  title="Logout"
+                  onPress={this.deleteToken.bind(this)}
+                  color="darkred"
+                />
+              </View>
             </View>
-          </View>
-        </ImageBackground>
-      </View>
-    );
+          </ImageBackground>
+        </View>
+      );
+    }
   }
 }
