2d2f0ddb8dee2f5cfa2feb05d9b88ec66161f832
John Lim <johnlim72@aol.com>
2018-04-11 11:32:06 -0400

    uichanges

---
+++ Views/Example.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { AppRegistry, StyleSheet, Text, View, Animated } from "react-native";
+
+const arr = [];
+for (var i = 0; i < 100; i++) {
+  arr.push(i);
+}
+
+export default class ExampleScreen extends React.Component {
+  constructor() {
+    super();
+    this.animatedValue = [];
+    arr.forEach(value => {
+      this.animatedValue[value] = new Animated.Value(0);
+    });
+  }
+
+  componentDidMount() {
+    this.animate();
+  }
+
+  animate() {
+    const animations = arr.map(item => {
+      return Animated.timing(this.animatedValue[item], {
+        toValue: 1,
+        duration: 4000
+      });
+    });
+    Animated.stagger(10, animations).start();
+  }
+
+  render() {
+    const animations = arr.map((a, i) => {
+      return (
+        <Animated.View
+          key={i}
+          style={{
+            opacity: this.animatedValue[a],
+            height: 20,
+            width: 20,
+            backgroundColor: "red",
+            marginLeft: 3,
+            marginTop: 3
+          }}
+        />
+      );
+    });
+    return <View style={styles.container}>{animations}</View>;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap"
+  }
+});
